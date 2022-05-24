4a99aaa4d47e1b22f92f983df903a0dcd8532914
Dennizz <root@ipaddr.nl>
2022-05-24 14:45:59 +0200

    cleanup

--- data.tf
+++ data.tf
@@ -1,11 +1,3 @@
-data "aviatrix_account" "default" {
-  account_name = local.account
-
-  depends_on = [
-    local.account,
-  ]
-}
-
 data "aviatrix_firewall_instance_images" "fw_images" {
   vpc_id = local.vpc.vpc_id
 
