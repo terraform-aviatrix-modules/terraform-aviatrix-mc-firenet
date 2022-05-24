4f43b2d4050ccd0aaf90da44287a3a1c436d6bc1
Dennizz <root@ipaddr.nl>
2022-05-24 14:45:16 +0200

    small fix

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aviatrix_account" "default" {
 }
 
 data "aviatrix_firewall_instance_images" "fw_images" {
-  vpc_id = local.vpc_id
+  vpc_id = local.vpc.vpc_id
 
   depends_on = [
     local.vpc,
