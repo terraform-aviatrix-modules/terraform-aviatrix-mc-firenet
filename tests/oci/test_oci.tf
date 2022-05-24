46afe3dfec707418d176d05d5ccbf03bf5504fa3
Dennizz <root@ipaddr.nl>
2022-05-24 14:42:55 +0200

    update tests

--- tests/oci/test_oci.tf
+++ tests/oci/test_oci.tf
@@ -11,7 +11,7 @@ terraform {
 
 module "transit_non_ha_oci" {
   source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
-  version = "2.0.0"
+  version = "2.1.1"
 
   cloud                  = "oci"
   name                   = "transit-non-ha-oci"
@@ -40,7 +40,7 @@ variable "custom_fw_names" {
 
 module "transit_ha_oci" {
   source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
-  version = "2.0.0"
+  version = "2.1.1"
 
   cloud                  = "oci"
   name                   = "transit-ha-oci"
